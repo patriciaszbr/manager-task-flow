63ce1a58374e9e256e54ce72c15b5ef52e6f0bab
Patrícia Souza <patricia.sz.br@gmail.com>
2022-07-28 22:51:13 -0300

    Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+
+import { Button } from './components/Button'
+
 export function App() {
 
 
   return (
-   <h1>Manager Task Flow</h1>
+    <>
+      <Button variant={'primary'} />
+      <Button variant={'secondary'} />
+      <Button variant={'danger'} />
+      <Button variant={'success'} />
+
+      
+    </>
       
   )
 }
