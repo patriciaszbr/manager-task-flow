77677fa11a53382c2ff034557d7dd43ad483379e
Patrícia Souza <patricia.sz.br@gmail.com>
2022-07-30 15:19:52 -0300

    Validando formularios

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import { Play } from "phosphor-react";
-
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
 
 import {
   CountdownContainer,
@@ -12,8 +13,18 @@ import {
   TaskInput,
 } from "./styles";
 
+const newCycleFormValidationSchemma = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  MinutesAmount: zod
+    .number()
+    .min(5, "O ciclo precisa ser de no min. 05 minutos")
+    .max(60, "O ciclo precisa ser de no max. 60 minutos"),
+});
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm();
+  const { register, handleSubmit, watch, formState } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchemma),
+  });
 
   function handleCreateNewCycle(data: any) {
     console.log(data);
